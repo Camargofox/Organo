9cb2d32064d4676a0220e97b01f93fb90d70d116
Camargo.S.O. <90009777+Camargofox@users.noreply.github.com>
2026-01-12 19:21:10 +0000

    adicionando as cores dos times no css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,42 @@ import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
 
 function App() {
+  const times = [
+    {
+      nome: 'Programação',
+      CorPrimaria: '#57c278',
+      CorSecundaria: '#d9f7d9'
+    },
+    {
+      nome: 'Front-End',
+      CorPrimaria: '',
+      CorSecundaria: ''
+    },{
+      nome: 'Data Science',
+      CorPrimaria: '',
+      CorSecundaria: ''
+    },{
+      nome: 'Devops',
+      CorPrimaria: '',
+      CorSecundaria: ''
+    },{
+      nome: 'Ux e Design',
+      CorPrimaria: '',
+      CorSecundaria: ''
+    },{
+      nome: 'Mobile',
+      CorPrimaria: '',
+      CorSecundaria: ''
+    },{
+      nome: 'Inovação e Gestão',
+      CorPrimaria: '',
+      CorSecundaria: ''
+    },
+    
+  ]
+
+
+
 
   const [colaboradores, setColaboradores] = useState([])
 
